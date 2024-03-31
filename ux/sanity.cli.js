8d7141915a57ec32c14f7e19bdee249858723d02
icodetree <icodetree@naver.com>
2024-03-31 23:48:50 +0900

    add init

---
+++ ux/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '3p9ylh2a',
+    dataset: 'production'
+  }
+})
---
+++ ux/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'ux',
+
+  projectId: '3p9ylh2a',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
